eaadabdc32f3615e0124616caa6e7e898f9e775e
Swaroop SM <swaroop.striker@gmail.com>
2014-06-22 19:59:26 +0530

    added some doc tags

--- src/tower.class.php
+++ src/tower.class.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * The Tower base class
+ *
+ * @package Tower
+ * @author Swaroop SM <swaroop.striker@gmail.com>
+ */
 class Tower {
 
   private $templateFile;
@@ -9,18 +15,37 @@ class Tower {
     $this->data = array();
   }
 
+  /**
+   * Sets the data variable
+   *
+   * @param string $key   The name for the variable
+   * @param mixed $value  The value for this named variable
+   */
   public function set($key, $value) {
     $this->data[$key] = $value;
   }
 
+  /**
+   * Sets the template file
+   *
+   * @param string $filename  Path of the template file to be used
+   */
   public function setTemplate($filename) {
     $this->templateFile = $filename;
   }
 
+  /**
+   * Render the template with the contents
+   */
   public function render() {
     echo $this->bufferize();
   }
 
+  /**
+   * Save the contents to a file
+   *
+   * @param string $filename  Filename to save the contents
+   */
   public function save($filename) {
     $contents = $this->getContents();
     $handle = fopen($filename, "w");
@@ -28,6 +53,11 @@ class Tower {
     fclose($handle);
   }
 
+  /**
+   * Get cotents after all the values are mapped to the template
+   *
+   * @return string Return all contents
+   */
   public function getContents() {
     return $this->bufferize();
   }
