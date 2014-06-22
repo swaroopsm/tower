2c24dea47b8b272c2c602610ba3b0ea0d6725a5b
Swaroop SM <swaroop.striker@gmail.com>
2014-06-22 19:02:25 +0530

    updated composer and also added initial tower class

---
+++ src/tower.class.php
@@ -0,0 +1,45 @@
+<?php
+
+class Tower {
+
+  private $templateFile;
+  private $data;
+
+  public function __construct() {
+    $this->data = array();
+  }
+
+  public function set($key, $value) {
+    $this->data[$key] = $value;
+  }
+
+  public function setTemplate($filename) {
+    $this->templateFile = $filename;
+  }
+
+  public function render() {
+    echo $this->bufferize();
+  }
+
+  public function save($filename) {
+    
+  }
+
+  public function getContents() {
+    return $this->bufferize();
+  }
+
+  private function bufferize() {
+    foreach($this->data as $key => $value) {
+      ${$key} = $value;
+    }
+
+    ob_start();
+    require $this->templateFile;
+    $contents = ob_get_contents();
+    ob_end_clean();
+
+    return $contents;
+  }
+
+}
